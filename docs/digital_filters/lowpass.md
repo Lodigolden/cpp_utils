90d1fa4f385e2bdbf89687f551d98bf8f74ce711
lodigolden <adamhagengruber@gmail.com>
2025-08-26 20:45:45 -0700

    Added documentation for the digital lowpass filter

---
+++ docs/digital_filters/lowpass.md
@@ -0,0 +1,19 @@
+# Digital Lowpass Filter
+The digital lowpass filter is obtained by converting the transfer function of an analog RC lowpass filter to the discrete domain via a forward Euler discertization.
+
+The analog lowpass filter transfer function:
+> $$ H(s) = { Y(s) \over X(s) } = { 1 \over 1+sRC } $$
+Where R is the resistance and C is the capacitance.
+
+The forward Euler discretization:
+> $$ s = { z-1 \over T_s } $$
+Where $ T_s $ is the system sampling time.
+
+The updated transfer function in the z-domain:
+> $$ H(z) = { Y(z) \over X(z) } = { T_s \over T_s+RC(1-z^-1) } $$
+
+The discrete output:
+> $$ y[n] = { T_s \over T_s+RC }x[n] + { RC \over T_s+RC }y[n-1] $$
+
+RC can be replaced by the system time constant $ T_c $:
+> $$ y[n] = { T_s \over T_s+T_c }x[n] + { T_c \over T_s+T_c }y[n-1] $$
--- tests/digital_filters/lowpass_tests.cpp
+++ tests/digital_filters/lowpass_tests.cpp
@@ -3,6 +3,7 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 #include "digital_filters/lowpass.h"
 
+#include <cmath>
 #include <cstdint>
 #include <gtest/gtest.h>
 
@@ -13,7 +14,8 @@ namespace
 {
 
 float default_sampling_speed{ 1.0 };
-float default_time_constant{ 2.0 };
+float default_cutoff_frequency{ 2.0 };
+float default_time_constant{ static_cast<float>(1 / (2 * M_PI * default_cutoff_frequency)) };
 
 } // namespace
 
@@ -21,40 +23,59 @@ float default_time_constant{ 2.0 };
 TEST(LowpassTest, Given_ADigitalLowpassFilter_When_SamplingSpeedIsSet_Then_SamplingSpeedIsCorrect)
 {
     // Given - A digital lowpass filter,
-    Digital_Filters::Digital_lowpass test_lowpass(default_sampling_speed, default_time_constant);
+    Digital_Filters::Digital_lowpass test_lowpass(default_sampling_speed, default_cutoff_frequency);
     EXPECT_FLOAT_EQ(test_lowpass.get_sampling_speed(), default_sampling_speed);
 
-    // When  - Sampling speed is set,
+    // When - Sampling speed is set,
     float updated_sampling_speed{ 5.0 };
     test_lowpass.set_sampling_speed(updated_sampling_speed);
     
-    // Then  - Sampling speed is correct.
+    // Then - Sampling speed is correct.
     EXPECT_FLOAT_EQ(test_lowpass.get_sampling_speed(), updated_sampling_speed);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-TEST(LowpassTest, Given_ADigitalLowpassFilter_When_TimeConstantIsSet_Then_TimeConstantIsCorrect)
+TEST(LowpassTest, Given_ADigitalLowpassFilter_When_CutoffFrequencyIsSet_Then_TimeConstantIsCorrect)
 {
     // Given - A digital lowpass filter,
-    Digital_Filters::Digital_lowpass test_lowpass(default_sampling_speed, default_time_constant);
+    Digital_Filters::Digital_lowpass test_lowpass(default_sampling_speed, default_cutoff_frequency);
     EXPECT_FLOAT_EQ(test_lowpass.get_time_constant(), default_time_constant);
 
-    // When  - Time constant is set,
-    float updated_time_constant{ 5.0 };
-    test_lowpass.set_time_constant(updated_time_constant);
+    // When - Cutoff frequency is set,
+    float updated_cutoff_frequency{ 100000.0 };
+    float updated_time_constant{ static_cast<float>(1 / (2 * M_PI * updated_cutoff_frequency)) };
+
+    test_lowpass.set_cutoff_frequency(updated_cutoff_frequency);
 
     // Then - Time constant is correct.
     EXPECT_FLOAT_EQ(test_lowpass.get_time_constant(), updated_time_constant);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST(
+    LowpassTest,
+    Given_ADigitalLowpassFilter_When_CutoffFrequencyIsSet_Then_CutoffFrequencyIsCorrect)
+{
+    // Given - A digital lowpass filter,
+    Digital_Filters::Digital_lowpass test_lowpass(default_sampling_speed, default_cutoff_frequency);
+    EXPECT_FLOAT_EQ(test_lowpass.get_cutoff_frequency(), default_cutoff_frequency);
+
+    // When - Time constant is set,
+    float updated_cutoff_frequency{ 5.0 };
+    test_lowpass.set_cutoff_frequency(updated_cutoff_frequency);
+
+    // Then - Time constant is correct.
+    EXPECT_FLOAT_EQ(test_lowpass.get_cutoff_frequency(), updated_cutoff_frequency);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 TEST(LowpassTest, Given_ADigitalLowpassFilter_When_NoTicksOccured_Then_OutputIsZero)
 {
     // Given - A digital lowpass filter,
-    Digital_Filters::Digital_lowpass test_lowpass(default_sampling_speed, default_time_constant);
+    Digital_Filters::Digital_lowpass test_lowpass(default_sampling_speed, default_cutoff_frequency);
 
-    // When  - No ticks occured,
-    // Then  - Output is zero.
+    // When - No ticks occured,
+    // Then - Output is zero.
     EXPECT_FLOAT_EQ(test_lowpass.get_output(), 0.0);    
 }
 
@@ -62,14 +83,14 @@ TEST(LowpassTest, Given_ADigitalLowpassFilter_When_NoTicksOccured_Then_OutputIsZ
 TEST(LowpassTest, Given_ADigitalLowpassFilter_When_ManyTicksOccured_Then_OutputIsExpected)
 {
     // Given - A digital lowpass filter,
-    Digital_Filters::Digital_lowpass test_lowpass(default_sampling_speed, default_time_constant);
+    Digital_Filters::Digital_lowpass test_lowpass(default_sampling_speed, default_cutoff_frequency);
 
-    // When  - Many ticks occured,
+    // When - Many ticks occured,
     for (uint8_t idx{ 0U }; idx < 100U; idx++)
     {
         static_cast<void>(test_lowpass.tick(1.0));
     }
 
-    // Then  - Output is expected.
+    // Then - Output is expected.
     EXPECT_FLOAT_EQ(test_lowpass.get_output(), 1.0);
 }
